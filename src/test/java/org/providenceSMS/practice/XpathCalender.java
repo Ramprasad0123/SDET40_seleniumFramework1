4d928671cfabe52261d8ca22d8892ea46bb8ce27
PC <PC@DESKTOP-8203IU4>
2022-09-21 12:48:43 +0530

    Format update

--- src/test/java/org/providenceSMS/practice/XpathCalender.java
+++ src/test/java/org/providenceSMS/practice/XpathCalender.java
@@ -17,7 +17,7 @@ public class XpathCalender {
 		WebDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-		//dat
+		//date
 		String requiredDate = "20";  //DD
 		String requiredMonth = "December";  //ex: MMMM--->July, MMM---->Jul, MM--->07
 		String requiredYear = "2022";  //YYYY
