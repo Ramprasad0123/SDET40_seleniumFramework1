35df96b4e93bbfba81bc73e800fe7ec86116b917
PC <PC@DESKTOP-8203IU4>
2022-09-21 14:49:19 +0530

    modify excel-1

--- src/test/java/org/providenceSMS/practice/FetchDataFromExcel.java
+++ src/test/java/org/providenceSMS/practice/FetchDataFromExcel.java
@@ -12,7 +12,7 @@ import org.apache.poi.ss.usermodel.WorkbookFactory;
 
 public class FetchDataFromExcel {
 	public static void main(String[] args) throws EncryptedDocumentException, IOException {
-		
+		//Start task..
 		//step1----> convert the physical file into java readable object
 				FileInputStream fxl=new FileInputStream("./src/test/resources/Commondatatest.xlsx");
 		
